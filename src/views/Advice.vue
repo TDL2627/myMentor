aa8bb741893e6c1d2d2fc013848eddbe68802104
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-08 13:10:35 +0200

    finished info

--- src/views/Advice.vue
+++ src/views/Advice.vue
@@ -1,22 +1,22 @@
 <template>
 <div class="advice">
-<h1>GENERAL INFORMATION</h1>
+<h1 class="heading">GENERAL INFORMATION</h1>
 
 <div class="container">
   <div class="row">
  <div class="col-lg-6">
-   
+   <router-link class="tabby" to="/time">TIME MANAGEMENT</router-link>
  </div>
  <div class="col-lg-6">
-   
+   <router-link class="tabby" to="/study">STUDY GUIDE</router-link>
  </div>
   </div>
   <div class="row">
     <div class="col-lg-6">
-   
+   <router-link class="tabby" to="/mental">MENTAL HELP</router-link>
  </div>
  <div class="col-lg-6">
-   
+   <router-link class="tabby" to="/exam">EXAM PREP</router-link>
  </div>
 
   </div>
@@ -32,5 +32,16 @@ export default {
 </script>
 
 <style scoped>
-
+.advice{
+  padding-top: 7%;
+}
+.row{
+    margin: 150px;
+}
+.tabby{
+    text-decoration: none;
+    color: rgb(52, 152, 235);
+    padding: 50px;
+    border: solid 5px  rgb(52, 152, 235);
+}
 </style>

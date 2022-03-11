7ae76699e266466a886e1feaa03cb65365974d1f
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-11 10:56:40 +0200

    added table

--- src/views/Shuttle.vue
+++ src/views/Shuttle.vue
@@ -2,6 +2,23 @@
 <div class="shuttles">
   <Nav/>
 <h1 class="heading">SHUTTLE TIMES</h1>
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+ 
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+ 
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+  
+  </tr>
+</table>
 </div>
 </template>
 
@@ -18,4 +35,13 @@ components:{
 .shuttles{
   padding-top: 7%;
 }
+table,td,tr,th{
+  color: white;
+  border: 1px solid white;
+}
+table{
+  width: 95%;
+  margin: 2%;
+}
+
 </style>

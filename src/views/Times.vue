0657e64bf8f614759d7d05d5c17b830d08bb740b
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-15 12:52:28 +0200

    made time table responsive

--- src/views/Times.vue
+++ src/views/Times.vue
@@ -3,6 +3,7 @@
   <Nav/>
   <h1 class="heading">TIME TABLE</h1>
   <img src="@/assets/0.png" class="img-fluid" alt="img">
+    <img src="@/assets/0.png" class="img-mob" alt="img">
 </div>
 </template>
 
@@ -17,7 +18,31 @@ components:{
 
 <style scoped>
 .times{
+  height: max-content !important;
+    overflow-y: hidden !important;
+    background: rgb(17, 20, 64);
+    width: 100%;
   padding-top: 7%;
   padding-bottom:7%
+}
+.img-fluid{
+  display: initial;
+}
+.img-mob{
+  display: none;
+}
+@media only screen and (max-width: 600px) {
+.times{
+    padding-top: 20%;
+    padding-bottom: 20%;
+}
+.img-fluid{
+  display: none;
+}
+.img-mob{
+  display: initial;
+  margin: 50px;
+}
+
 }
 </style>

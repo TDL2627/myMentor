2e0b9f5071a45d869eef623ece9805a910726dcb
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-19 14:10:28 +0200

    added about

--- src/router/index.js
+++ src/router/index.js
@@ -113,6 +113,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue')
   },
   {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
     path: '/notepad',
     name: 'Notepad',
     // route level code-splitting
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+<h1 class="heading">ABOUT</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>

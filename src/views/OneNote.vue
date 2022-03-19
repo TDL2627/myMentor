034e466d3d070f8c0d5825ab15d0f656afdd4ab2
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-19 12:45:01 +0200

    added loader to one note

--- src/views/OneNote.vue
+++ src/views/OneNote.vue
@@ -162,4 +162,48 @@ h2{
 ul{
   list-style: none;
 }
+/* loader */
+
+.half-circle-spinner, .half-circle-spinner * {
+      box-sizing: border-box;
+    }
+
+    .half-circle-spinner {
+      z-index: 2627;
+      width: 60px;
+      height: 60px;
+      border-radius: 100%;
+      position: fixed;
+      top:45%;
+      left: 50%;
+    }
+
+    .half-circle-spinner .circle {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: 100%;
+      border: calc(60px / 10) solid transparent;
+    }
+
+    .half-circle-spinner .circle.circle-1 {
+      border-top-color: white;
+      animation: half-circle-spinner-animation 1s infinite;
+    }
+
+    .half-circle-spinner .circle.circle-2 {
+      border-bottom-color: #1d92ff;
+      animation: half-circle-spinner-animation 1s infinite alternate;
+    }
+
+    @keyframes half-circle-spinner-animation {
+      0% {
+        transform: rotate(0deg);
+
+      }
+      100%{
+        transform: rotate(360deg);
+      }
+    }
 </style>
